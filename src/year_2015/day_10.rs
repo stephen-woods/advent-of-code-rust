c7d921259e8cd3ae65c31928895a757ebf1599cb
Stephen Woods <stephen.woods@altarasystems.com>
2023-08-10 23:33:11 -0400

    2015 10

--- src/year_2015/day_10.rs
+++ src/year_2015/day_10.rs
@@ -13,56 +13,57 @@ pub fn run() {
     let now = SystemTime::now();
     let answer_b = part_b();
     let duration = now.elapsed().expect("Elapsed failed");
-    println!("Huh?");
+    println!("Now, starting again with the digits in your puzzle input, apply this process 50 times. What is the length of the new result?");
     println!(" {}", answer_b);
     println!(" in {}ms", duration.as_millis());
 }
 
-
-fn part_a() -> u32 {
-    match parse_digits(INPUT_A) {
-        Ok(r) => println!("{:?}",r),
-        Err(e) => println!("{e}")
+fn part_a() -> usize {
+    let mut say = String::from(INPUT_A);
+    for _ in 0..40 {
+        let look = parse_digits(&say);
+        say = look_say(look);
     }
-    3
+
+    say.len()
 }
 
+fn part_b() -> usize {
+    let mut say = String::from(INPUT_A);
+    for _ in 0..50 {
+        let look = parse_digits(&say);
+        say = look_say(look);
+    }
 
-fn part_b() -> u32 {
-    3
+    say.len()
 }
 
-fn parse_digits(look: &str) -> Result<Vec<u32>, String> {
-    look
+fn parse_digits(look: &str) -> Vec<u32> {
+    let ret: Result<Vec<u32>, String> = look
         .chars()
-        .map(|c| c
-            .to_digit(10)
-            .ok_or(String::from("Error: Input contains non digits"))
-        )
-        .collect()
+        .map(|c| {
+            c.to_digit(10)
+                .ok_or(String::from("Error: Input contains non digits"))
+        })
+        .collect();
+    ret.unwrap()
 }
 
-
-
-
-fn look_say(look: Vec<u32>) -> Result<String, String> {
-    if look.is_empty() {
-        return Err(String::from("Error: Input is empty"));
-    }
-
+fn look_say(look: Vec<u32>) -> String {
     let size = look.len();
     let mut i = 0;
+    let mut ret = String::new();
     while i < size {
-        let mut count = 1;
-        let j = i;
-        while j < size && look[i] == look[j] {
-            count += 1
+        let mut count = 0u32;
+        let c = look[i];
+        while i < size && look[i] == c {
+            count += 1;
+            i += 1;
         }
-
-        i += 1;
+        ret.push(char::from_digit(count, 10).unwrap());
+        ret.push(char::from_digit(c, 10).unwrap());
     }
-    Ok(String::from("Ok"))
+    ret
 }
 
 const INPUT_A: &str = "1321131112";
-
