c13cb04fdb71f111903952bdc33779665aea94c4
Woods, Stephen (Contractor) <stephen_woods@cable.comcast.com>
2025-11-15 23:30:45 -0500

    Added rust test modules for unit tests

--- src/year_2015/day_04.rs
+++ src/year_2015/day_04.rs
@@ -1,26 +1,25 @@
-// --- Day 4: The Ideal Stocking Stuffer ---
-// Santa needs help mining some AdventCoins (very similar to bitcoins) to use as gifts for all the
-// economically forward-thinking little girls and boys.
-//
-// To do this, he needs to find MD5 hashes which, in hexadecimal, start with at least five zeroes.
-// The input to the MD5 hash is some secret key (your puzzle input, given below) followed by a
-// number in decimal. To mine AdventCoins, you must find Santa the lowest positive number (no
-// leading zeroes: 1, 2, 3, ...) that produces such a hash.
-//
-// For example:
-//
-// - If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts
-//   with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
-// - If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting
-//   with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
-//
-// Your puzzle answer was 282749.
-//
-// --- Part Two ---
-// Now find one that starts with six zeroes.
-//
-// Your puzzle answer was 9962624.
-
+/// --- Day 4: The Ideal Stocking Stuffer ---
+/// Santa needs help mining some AdventCoins (very similar to bitcoins) to use as gifts for all the
+/// economically forward-thinking little girls and boys.
+///
+/// To do this, he needs to find MD5 hashes which, in hexadecimal, start with at least five zeroes.
+/// The input to the MD5 hash is some secret key (your puzzle input, given below) followed by a
+/// number in decimal. To mine AdventCoins, you must find Santa the lowest positive number (no
+/// leading zeroes: 1, 2, 3, ...) that produces such a hash.
+///
+/// For example:
+///
+/// - If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts
+///   with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
+/// - If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting
+///   with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+///
+/// Your puzzle answer was 282749.
+///
+/// --- Part Two ---
+/// Now find one that starts with six zeroes.
+///
+/// Your puzzle answer was 9962624.
 use md5::{Digest, Md5};
 use std::time::SystemTime;
 
@@ -68,14 +67,19 @@ fn mine(prefix: &str) -> u64 {
 
 const INPUT_A: &str = "yzbqklnj";
 
-#[test]
-fn test_a() {
-    let result = part_a();
-    assert_eq!(result, 282749);
-}
+#[cfg(test)]
+mod tests {
+    use crate::year_2015::day_04::{part_a, part_b};
 
-#[test]
-fn test_b() {
-    let result = part_b();
-    assert_eq!(result, 9962624);
+    #[test]
+    fn test_a() {
+        let result = part_a();
+        assert_eq!(result, 282749);
+    }
+
+    #[test]
+    fn test_b() {
+        let result = part_b();
+        assert_eq!(result, 9962624);
+    }
 }
